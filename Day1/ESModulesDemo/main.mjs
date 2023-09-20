0384350c961fae5654bd7e1900765c6d353f3c00
Sujatabatra <sujatabatra@hotmail.com>
2023-09-20 14:52:43 +0530

    Add files via upload

---
+++ Day1/ESModulesDemo/main.mjs
@@ -0,0 +1,8 @@
+import math from "./math-esm.mjs"
+
+const {add,subtract}=math;
+
+console.log(add(10,20));
+console.log(subtract(45,3));
+// console.log(math.add(10,20));
+// console.log(math.subtract(45,3));
---
+++ Day1/ESModulesDemo/math-esm.mjs
@@ -0,0 +1,18 @@
+const add =(a,b)=>{
+    return a+b;
+}
+const subtract=(a,b)=>{
+    return a-b;
+}
+
+export default{
+    add,
+    subtract
+}
+// export default{
+//     add:add,
+//     subtract:subtract
+// }
+
+// export default add;
+// export default subtract;
