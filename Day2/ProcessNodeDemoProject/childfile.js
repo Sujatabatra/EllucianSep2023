8a439af9b3bc67e0bbfafd7193d81a8b230a4d89
Sujatabatra <sujatabatra@hotmail.com>
2023-09-21 13:23:20 +0530

    Add files via upload

--- Day2/ProcessNodeDemoProject/childfile.js
+++ Day2/ProcessNodeDemoProject/childfile.js
@@ -1,1 +1,3 @@
-console.log(`Child Process ${process.argv} executed`);
+const { argv } = require("process");
+
+console.log(`Child Process ${process.argv[argv.length-1]} executed`);
--- Day2/ProcessNodeDemoProject/parentfile.js
+++ Day2/ProcessNodeDemoProject/parentfile.js
@@ -2,7 +2,7 @@ const fs=require("node:fs");
 const child_process=require('child_process');
 
 for(var i=0;i<3;i++){
-    var workProcess=child_process.exec('node childfile.js'+i,
+    var workerProcess=child_process.exec('node childfile.js '+i,
     (error,stdout,stderr)=>{
         if(error){
             console.log(error.stack);
@@ -12,7 +12,7 @@ for(var i=0;i<3;i++){
         console.log(`stdout : ${stdout}`);
         console.log(`stderr : ${stderr}`);
     })
-    workProcess.on("exit",(code)=>{
+    workerProcess.on("exit",(code)=>{
         console.log(`Child process exited with exit code ${code}`);
     })
 }
